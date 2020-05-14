ea7d0b16fed384f3a6280dcf02572d960d060a7a
김연희 <yonee@ogqcorp.com>
2020-05-14 18:02:27 +0900

    modal component 추가

---
+++ components/modal.vue
@@ -0,0 +1,64 @@
+<template>
+  <v-dialog
+    v-model="isActive"
+    max-width="500"
+  >
+    <v-card>
+      <v-card-title class="headline" align="center">{{ data.status ? '대여가능' : data.rentalName }}</v-card-title>
+      <v-img
+        class="white--text align-end"
+        :src="data.status ? require('~/assets/images/able_card.png') : require('~/assets/images/disable_card.png')"
+        width="100%"></v-img>
+      <v-avatar
+        color="orange">
+        <span class="white--text headline">{{ data.id }}</span>
+      </v-avatar>
+      <v-card-actions>
+        <v-spacer></v-spacer>
+        <v-btn
+          color="green darken-1"
+          text
+          @click="rentalCard()"
+        >
+          대여
+        </v-btn>
+        <v-btn
+          color="green darken-1"
+          text
+          @click="returnCard()"
+        >
+          반납
+        </v-btn>
+      </v-card-actions>
+    </v-card>
+  </v-dialog>
+</template>
+
+<script>
+    export default {
+      name: 'modal',
+      props: {
+        isActive: {
+          type: Boolean,
+          default: false
+        },
+        data: {
+          type: Object,
+          default: () => {}
+        }
+      },
+      methods: {
+        rentalCard() {
+          // 카드대여 actions
+          this.$emit('close')
+        },
+        returnCard () {
+          // 카드반납 actions
+          this.$emit('close')
+        }
+      }
+    }
+</script>
+
+<style scoped>
+</style>
--- pages/card/index.vue
+++ pages/card/index.vue
@@ -1,33 +1,35 @@
 <template>
-  <v-container fluid>
-    <v-row>
-      <v-col cols="12">
-        <v-row
-          :align="alignment"
-          :justify="justify"
-          class="grey lighten-5"
-          style="height: 100%">
-          <v-card
-            v-for="card in cardList"
-            :key="card.id"
-            :color="card.status ? '#26c6da' : 'pink darken-1'"
-            class="ma-5 pa-12"
-            max-width="200"
-            outlined
-            tile>
-            <v-avatar
-              color="orange">
-              <span class="white--text headline">{{ card.id }}</span>
-            </v-avatar>
-            <v-card-title>
-              <v-btn v-if="card.status" text>대여가능</v-btn>
-              <v-btn v-else text>{{ card.rentalName }}</v-btn>
-            </v-card-title>
-          </v-card>
-        </v-row>
-      </v-col>
-    </v-row>
-  </v-container>
+  <div>
+    <v-container fluid>
+      <v-row>
+        <v-col cols="12">
+          <v-row
+            :align="alignment"
+            :justify="justify"
+            class="grey lighten-5"
+            style="height: 100%">
+            <v-card
+              v-for="card in cardList"
+              :key="card.id"
+              :color="card.status ? '#26c6da' : 'pink darken-1'"
+              class="ma-5 pa-12"
+              max-width="200"
+              outlined
+              tile>
+              <v-avatar
+                color="orange">
+                <span class="white--text headline">{{ card.id }}</span>
+              </v-avatar>
+              <v-card-title>
+                <v-btn @click="clickCard(card)">{{ card.status ? '대여가능' : card.rentalName }}</v-btn>
+              </v-card-title>
+            </v-card>
+          </v-row>
+        </v-col>
+      </v-row>
+    </v-container>
+    <modal :isActive="isActiveModal" :data="card" @close="toggleModal"></modal>
+  </div>
 </template>
 
 <script>
@@ -35,12 +37,17 @@ import * as cardStore from '@/store/card'
 import * as cardGetters from '@/store/card/getters'
 import * as cardActions from '@/store/card/actions'
 
+import Modal from '@/components/modal'
+
 export default {
   name: 'Card',
+  components: { Modal },
   data () {
     return {
       alignment: 'center',
-      justify: 'center'
+      justify: 'center',
+      isActiveModal: false,
+      card: {}
     }
   },
   computed: {
@@ -57,6 +64,14 @@ export default {
     ]),
     async getTest () {
       await this[cardActions.INIT_CARD_LIST]()
+    },
+    clickCard (card) {
+      this.toggleModal()
+      // 해당 카드 정보 저장
+      this.card = card
+    },
+    toggleModal () {
+      this.isActiveModal = !this.isActiveModal
     }
   }
 }
