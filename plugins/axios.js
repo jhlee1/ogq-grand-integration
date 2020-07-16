ba0b27f9fa9cf60dc1eb38638aa15769c9f65965
이세헌 <dltpgjs87@gmail.com>
2020-07-16 17:37:40 +0900

    login setting

--- plugins/axios.js
+++ plugins/axios.js
@@ -1,7 +1,5 @@
 export default function ({ $axios, redirect }) {
   $axios.setBaseURL('http://grand.market.ogqcorp.com:8080')
-
-  console.log('set baseUrl')
   $axios.onError(error => {
     if(error.response.status === 500) {
       redirect('/sorry')
