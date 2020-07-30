4a2368e9cae4583e3ad67eba690927ba45c0cfa8
이세헌 <dltpgjs87@gmail.com>
2020-07-30 18:48:53 +0900

    toast chart 추가

---
+++ pages/dashboard/chart/index.vue
@@ -0,0 +1,38 @@
+<template>
+  <div>
+    <client-only>
+      <bar-chart :data="chartData"/>
+    </client-only>
+  </div>
+</template>
+
+<script>
+
+	export default {
+    name: 'dashboard-chart',
+    data() {
+      return {
+        chartData: { // for 'data' prop of 'bar-chart'
+          categories: ['July', 'Aug', 'Sep', 'Oct', 'Nov'],
+          series: [
+            {
+                name: 'Budget',
+                data: [3000, 5000, 7000, 6000, 4000]
+            },
+            {
+                name: 'Income',
+                data: [1000, 7000, 2000, 5000, 3000]
+            }
+          ]
+        }
+      }
+    },
+		mounted() {},
+		methods: {
+		}
+	}
+</script>
+
+<style scoped>
+
+</style>
--- pages/dashboard/index.vue
+++ pages/dashboard/index.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <button label="TEST" @click="getGoogleAnalytics">test</button>
+<button label="TEST" @click="getGoogleAnalytics">test</button>
   </div>
 </template>
 
@@ -13,54 +13,55 @@
 		methods: {
 			async getGoogleAnalytics() {
 				try {
-						const res = await gAPI.getAnalytics()
-						console.log('res', res)
+                    const test = await this.gapiAnalyticsInit()
+                    const res = await gAPI.getAnalytics()
+                    console.log('res', res)
 				} catch (err) {
-						console.error('gapi getAnalytics', err)
+                    console.error('gapi getAnalytics', err)
 				}
 			},
 			async gapiAuthIInit() {
 				try {
-						const res = await gapi.auth2.init({
-						// 'clientId': '176109249735-uorsueki6n45tbnfkvvidicf6k736ink.apps.googleusercontent.com',
-						'clientId': '759830993627-j9p2qbkg3f9559tcs16pldih0d5bp5no.apps.googleusercontent.com',
-						'scope': 'profile'
-						})
-						console.log('res', res)
-						this.auth2 = res
+                    const res = await gapi.auth2.init({
+                    'clientId': '176109249735-uorsueki6n45tbnfkvvidicf6k736ink.apps.googleusercontent.com',
+                    //'clientId': '759830993627-j9p2qbkg3f9559tcs16pldih0d5bp5no.apps.googleusercontent.com',
+                    'scope': 'profile'
+                    })
+                    console.log('res', res)
+                    this.auth2 = res
 				} catch (err) {
-						console.error('gapiAuthIInit', err)
+                    console.error('gapiAuthIInit', err)
 				}
 			},
 			async getGoogleProfile() {
 				try {
-						const gUser = await this.auth2.currentUser.get()
-						console.log('gUser', gUser)
-						const profile = gUser.getBasicProfile()
-						console.log('profile', profile)
-						console.log('ID: ' + profile.getId())
-						console.log('Full Name: ' + profile.getName())
-						console.log('Given Name: ' + profile.getGivenName())
-						console.log('Family Name: ' + profile.getFamilyName())
-						console.log('Image URL: ' + profile.getImageUrl())
-						console.log('Email: ' + profile.getEmail())
+                    const gUser = await this.auth2.currentUser.get()
+                    console.log('gUser', gUser)
+                    const profile = gUser.getBasicProfile()
+                    console.log('profile', profile)
+                    console.log('ID: ' + profile.getId())
+                    console.log('Full Name: ' + profile.getName())
+                    console.log('Given Name: ' + profile.getGivenName())
+                    console.log('Family Name: ' + profile.getFamilyName())
+                    console.log('Image URL: ' + profile.getImageUrl())
+                    console.log('Email: ' + profile.getEmail())
 				} catch (err) {
-						console.error('getGoogleProfile', err)
+                    console.error('getGoogleProfile', err)
 				}
 			},
 			async gapiAnalyticsInit() {
 				try {
-						const res = await gapi.client.init({
-						'apiKey': 'AIzaSyCKbpkZPNqGGti_r0XMGxQo7BFWLgh3yHo',
-						'discoveryDocs': [ 'https://analyticsreporting.googleapis.com/$discovery/rest?version=v4' ],
-						// 'clientId': '176109249735-uorsueki6n45tbnfkvvidicf6k736ink.apps.googleusercontent.com',
-						'clientId': '759830993627-j9p2qbkg3f9559tcs16pldih0d5bp5no.apps.googleusercontent.com',
-						'scope': 'https://www.googleapis.com/auth/analytics.readonly'
-						})
-						console.log('res', res)
-						this.client = res
+                    const res = await gapi.client.init({
+                    'apiKey': 'AIzaSyCKbpkZPNqGGti_r0XMGxQo7BFWLgh3yHo',
+                    'discoveryDocs': [ 'https://analyticsreporting.googleapis.com/$discovery/rest?version=v4' ],
+                    'clientId': '176109249735-uorsueki6n45tbnfkvvidicf6k736ink.apps.googleusercontent.com',
+                    // 'clientId': '759830993627-j9p2qbkg3f9559tcs16pldih0d5bp5no.apps.googleusercontent.com',
+                    'scope': 'https://www.googleapis.com/auth/analytics.readonly'
+                    })
+                    console.log('res', res)
+                    this.client = res
 				} catch (err) {
-						console.error('gapiAnalyticsInit', err)
+                    console.error('gapiAnalyticsInit', err)
 				}
 			},
 			async getAnalytics() {
